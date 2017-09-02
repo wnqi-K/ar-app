eea022f61231d371025f3c3c880c53f4724f220d
wnqi-K <ricky.kuang96@gmail.com>
2017-09-02 15:40:46 +1000

    Delete a useless button.

--- ARRRRR/app/src/main/java/com/comp30022/arrrrr/MainActivity.java
+++ ARRRRR/app/src/main/java/com/comp30022/arrrrr/MainActivity.java
@@ -7,19 +7,10 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mBtnMap = null;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mBtnMap = (Button)findViewById(R.id.activity_main_btn_map);
-        mBtnMap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(view.getContext(), MapsActivity.class));
-            }
-        });
     }
 
     public void emailLogin(View view) {
