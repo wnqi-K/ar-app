d0141cfc1cef08fc4a54055b9e285febc660473b
jayai2014 <dafu.ai819@gmail.com>
2017-09-02 15:03:39 +1000

    remove button

--- ARRRRR/app/src/main/java/com/comp30022/arrrrr/MainActivity.java
+++ ARRRRR/app/src/main/java/com/comp30022/arrrrr/MainActivity.java
@@ -4,22 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button mBtnMap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mBtnMap = (Button)findViewById(R.id.activity_main_btn_map);
-        mBtnMap.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(view.getContext(), MapsActivity.class));
-            }
-        });
     }
 
     public void emailLogin(View view) {
