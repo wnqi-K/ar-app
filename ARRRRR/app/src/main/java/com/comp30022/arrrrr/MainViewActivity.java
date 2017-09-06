6bc2175626379bc26597a3d1145b12d6f3cd7e7c
wnqi-K <ricky.kuang96@gmail.com>
2017-09-06 15:49:49 +1000

    Logout functionality done. Now can sign out  email-logging-in account, and switch back to the login interface.

--- ARRRRR/app/src/main/java/com/comp30022/arrrrr/MainViewActivity.java
+++ ARRRRR/app/src/main/java/com/comp30022/arrrrr/MainViewActivity.java
@@ -30,7 +30,7 @@ public class MainViewActivity extends AppCompatActivity implements
 
             switch (item.getItemId()) {
                 case R.id.navigation_friends:
-                    switchToFragmentUsers();
+                    switchToFragmentFriends();
                     return true;
 
                 case R.id.navigation_home:
@@ -48,7 +48,7 @@ public class MainViewActivity extends AppCompatActivity implements
     /**
      * Switch the current fragment to friends fragment.
      */
-    private void switchToFragmentUsers() {
+    private void switchToFragmentFriends() {
         FragmentManager manager = getFragmentManager();
         manager.beginTransaction().replace(R.id.fragment_container,
                 UsersFragment.newInstance(1)).commit();
