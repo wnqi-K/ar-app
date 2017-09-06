44b3a635f4a2fbac8b093e2ba8d5c509c171ae96
wnqi-K <ricky.kuang96@gmail.com>
2017-09-06 16:00:13 +1000

    Delete getIntent method from mainActivity.

--- ARRRRR/app/src/main/java/com/comp30022/arrrrr/MainActivity.java
+++ ARRRRR/app/src/main/java/com/comp30022/arrrrr/MainActivity.java
@@ -11,11 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if (getIntent().getBooleanExtra("LOGOUT", false))
-        {
-            finish();
-        }
     }
 
     public void emailLogin(View view) {
