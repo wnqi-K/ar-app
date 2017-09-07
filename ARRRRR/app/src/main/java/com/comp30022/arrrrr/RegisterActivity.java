b902a1e19d77ee15d347c090b4819400621109b8
zijies1 <546641464@qq.com>
2017-09-07 17:38:05 +1000

    fix registeration bugs

--- ARRRRR/app/src/main/java/com/comp30022/arrrrr/RegisterActivity.java
+++ ARRRRR/app/src/main/java/com/comp30022/arrrrr/RegisterActivity.java
@@ -11,7 +11,6 @@ import com.comp30022.arrrrr.R;
 import com.comp30022.arrrrr.RegisterFragment;
 
 public class RegisterActivity extends AppCompatActivity {
-    private Toolbar mToolbar;
 
     public static void startActivity(Context context) {
         Intent intent = new Intent(context, RegisterActivity.class);
@@ -22,18 +21,11 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        bindViews();
         init();
     }
 
-    private void bindViews() {
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-    }
 
     private void init() {
-        // set the toolbar
-        setSupportActionBar(mToolbar);
-
         // set the register screen fragment
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout_content_register,
