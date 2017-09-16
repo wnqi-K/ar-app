e6674e344420018940fa5ec2b245474316096762
zijies1 <546641464@qq.com>
2017-09-16 15:34:36 +1000

    fix user/users models confliction

--- ARRRRR/app/src/main/java/com/comp30022/arrrrr/models/User.java
+++ ARRRRR/app/src/main/java/com/comp30022/arrrrr/models/User.java
@@ -9,6 +9,10 @@ public class User {
     public String uid;
     public String email;
     public String firebaseToken;
+    public String username;
+    public String phoneNum;
+    public String gender;
+    public String address;
     public ArrayList<String> friendsIDs;
 
     public User(){
@@ -25,4 +29,20 @@ public class User {
     public void addFriend(User friend){
         friendsIDs.add(friend.uid);
     }
+
+    public String getUsername(){ return username; }
+
+    public void setUsername(String username){ this.username = username;}
+
+    public String getPhoneNum(){ return phoneNum;}
+
+    public void setPhoneNum(String phoneNo){ this.phoneNum = phoneNo; }
+
+    public String getGender(){ return gender; }
+
+    public void setGender(String gender){ this.gender = gender; }
+
+    public String getAddress(){ return address; }
+
+    public void setAddress(String address){ this.address = address; }
 }
