3d77fa574dc37f2ce99dcac916a11138cab4008d
Xiaoyu Guo <xiaoyuguo@Yu.gateway>
2017-09-18 19:21:49 +1000

    Add a new package called ar which contains Ar utils(sensor & location)

---
+++ ARRRRR/app/src/main/java/com/comp30022/arrrrr/ArViewActivity.java
@@ -0,0 +1,13 @@
+package com.comp30022.arrrrr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ar_view);
+    }
+}
---
+++ ARRRRR/app/src/main/java/com/comp30022/arrrrr/ar/OnAzimuthChangedListener.java
@@ -0,0 +1,8 @@
+package com.comp30022.arrrrr.ar;
+
+/**
+ * Created by krzysztofjackowski on 24/09/15.
+ */
+public interface OnAzimuthChangedListener {
+    void onAzimuthChanged(float azimuthFrom, float azimuthTo);
+}
