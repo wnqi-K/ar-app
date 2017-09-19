66e93ea20ed028fa227251a0760f97a137886ab5
zijies1 <546641464@qq.com>
2017-09-19 12:41:41 +1000

    finish partial firebase cloud messaging service

--- ARRRRR/app/src/main/java/com/comp30022/arrrrr/services/MyFirebaseMessagingService.java
+++ ARRRRR/app/src/main/java/com/comp30022/arrrrr/services/MyFirebaseMessagingService.java
@@ -1,9 +1,24 @@
 package com.comp30022.arrrrr.services;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 
+import com.comp30022.arrrrr.MainActivity;
+import com.comp30022.arrrrr.MainViewActivity;
+import com.comp30022.arrrrr.PushNotificationEvent;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by rondo on 9/19/17.
@@ -13,11 +28,83 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "FCM Service";
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // TODO: Handle FCM messages here.
+
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+            String title = remoteMessage.getData().get("title");
+            String message = remoteMessage.getData().get("text");
+            String username = remoteMessage.getData().get("username");
+            String uid = remoteMessage.getData().get("uid");
+            String fcmToken = remoteMessage.getData().get("fcm_token");
+
+            // Don't show notification if chat activity is open.
+            if (!MainActivity.isChatActivityOpen()) {
+                sendNotification(remoteMessage);
+
+            } else {
+                EventBus.getDefault().post(new PushNotificationEvent(title,
+                        message,
+                        username,
+                        uid,
+                        fcmToken));
+            }
+        }
+
+
+
+
         // If the application is in the foreground handle both data and notification messages here.
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated.
         Log.d(TAG, "From: " + remoteMessage.getFrom());
         Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
     }
+
+    private void sendNotification(RemoteMessage remoteMessage){
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this);
+        mBuilder.setSmallIcon(android.support.v7.appcompat.R.drawable.notification_template_icon_bg);
+        mBuilder.setContentTitle("First notification");
+        mBuilder.setContentText(remoteMessage.getNotification().getBody());
+
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, MainViewActivity.class);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your app to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // Adds the back stack for the Intent (but not the Intent itself)
+        stackBuilder.addParentStack(MainViewActivity.class);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(
+                        0,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // build notification
+        int mNotificationId = createID();
+        mNotificationManager.notify(mNotificationId, mBuilder.build());
+    }
+
+
+    /**
+     * create unique id for notification id
+     * */
+    private int createID(){
+        Date now = new Date();
+        int id = Integer.parseInt(new SimpleDateFormat("ddHHmmss",  Locale.US).format(now));
+        return id;
+    }
 }
+
+
