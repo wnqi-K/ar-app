46019ad9d26612c89e3688fc1394904984159bea
wnqi-K <ricky.kuang96@gmail.com>
2017-09-19 14:47:56 +1000

    - set up an example for search.

--- ARRRRR/app/src/main/java/com/comp30022/arrrrr/AddingFriendsActivity.java
+++ ARRRRR/app/src/main/java/com/comp30022/arrrrr/AddingFriendsActivity.java
@@ -2,17 +2,68 @@ package com.comp30022.arrrrr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import com.comp30022.arrrrr.adapters.ListViewAdapter;
+import com.comp30022.arrrrr.models.AnimalNames;
+
+import java.util.ArrayList;
 
 /**
  * Adding new friends by searching the precise user email account.
  *
  * Created by Wenqiang Kuang on 16/09/2017.
  */
-public class AddingFriendsActivity extends AppCompatActivity {
+public class AddingFriendsActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+    // Declare Variables
+    ListView list;
+    ListViewAdapter adapter;
+    SearchView editsearch;
+    String[] animalNameList;
+    ArrayList<AnimalNames> arraylist = new ArrayList<AnimalNames>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_adding_new_friends);
+        setContentView(R.layout.activity_adding_friends);
+
+        // Generate sample data
+
+        animalNameList = new String[]{"Lion", "Tiger", "Dog",
+                "Cat", "Tortoise", "Rat", "Elephant", "Fox",
+                "Cow","Donkey","Monkey"};
+
+        // Locate the ListView in listview_main.xml
+        list = (ListView) findViewById(R.id.listview);
+
+        for (int i = 0; i < animalNameList.length; i++) {
+            AnimalNames animalNames = new AnimalNames(animalNameList[i]);
+            // Binds all strings into an array
+            arraylist.add(animalNames);
+        }
+
+        // Pass results to ListViewAdapter Class
+        adapter = new ListViewAdapter(this, arraylist);
+
+        // Binds the Adapter to the ListView
+        list.setAdapter(adapter);
+
+        // Locate the EditText in listview_main.xml
+        editsearch = (SearchView) findViewById(R.id.search);
+        editsearch.setOnQueryTextListener(this);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        String text = newText;
+        adapter.filter(text);
+        return false;
     }
 }
