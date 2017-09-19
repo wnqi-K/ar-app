0b9eced3d3fbbbdfc3b8e70b57e91d4cfc2bc421
zijies1 <546641464@qq.com>
2017-09-20 00:37:57 +1000

    rebuild add user to firebase database

--- ARRRRR/app/src/main/java/com/comp30022/arrrrr/MainViewActivity.java
+++ ARRRRR/app/src/main/java/com/comp30022/arrrrr/MainViewActivity.java
@@ -12,11 +12,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.comp30022.arrrrr.FriendManagement.FriendManagement;
 import com.comp30022.arrrrr.FriendManagement.requestFirebaseUsers;
 import com.comp30022.arrrrr.database.DatabaseManager;
 import com.comp30022.arrrrr.models.User;
+import com.comp30022.arrrrr.utils.Constants;
+import com.comp30022.arrrrr.utils.SharedPrefUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Main view of the application after user has logged in. This contains three
@@ -32,6 +41,7 @@ public class MainViewActivity extends AppCompatActivity implements
 
     private DatabaseManager mDatabaseManager;
     private requestFirebaseUsers mRequestUsers;
+    private FirebaseAuth mAuth;
 
     public requestFirebaseUsers getRequestUsers() {
         return mRequestUsers;
@@ -50,6 +60,9 @@ public class MainViewActivity extends AppCompatActivity implements
         // Set the default fragment to be map
         switchToFragmentHome();
 
+        // add user to database
+        mAuth = FirebaseAuth.getInstance();
+        addUserToDatabase(mAuth.getCurrentUser());
         // Get all users from database
         mDatabaseManager = new DatabaseManager(this,null,null,1);
         this.mRequestUsers = new requestFirebaseUsers(mDatabaseManager);
@@ -190,5 +203,32 @@ public class MainViewActivity extends AppCompatActivity implements
     public static void startActivity(Context context) {
         Intent intent = new Intent(context, MainViewActivity.class);
         context.startActivity(intent);
+    }
+
+
+    private void addUserToDatabase(FirebaseUser firebaseUser){
+        String uid = firebaseUser.getUid();
+        User user = new User(uid,
+                firebaseUser.getEmail(),
+                new SharedPrefUtil(this).getString(Constants.ARG_FIREBASE_TOKEN));
+        DatabaseReference mRef = FirebaseDatabase.getInstance().getReference();
+        mRef.child(Constants.ARG_USERS)
+                .child(uid)
+                .setValue(user)
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(),
+                                    Constants.ARG_FAILURE, Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(),
+                                    Constants.ARG_SUCCESS, Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
+
     }
 }
--- ARRRRR/app/src/main/java/com/comp30022/arrrrr/utils/Constants.java
+++ ARRRRR/app/src/main/java/com/comp30022/arrrrr/utils/Constants.java
@@ -16,6 +16,9 @@ public class Constants {
     public static final String ARG_GENDER = "gender";
     public static final String ARG_ADDRESS = "address";
     public static final String ARG_EMAIL= "email";
+    public static final String ARG_SUCCESS = "success";
+    public static final String ARG_FAILURE = "failure";
+
 
 
 }
