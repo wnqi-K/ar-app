78ddc4271f5998fbf313cedd6a9e258e22eb03a2
Xiaoyu Guo <guox2@student.unimelb.edu.au>
2017-09-23 20:26:42 +1000

    Add comments and fixed some bugs

---
+++ ARRRRR/app/src/main/java/com/comp30022/arrrrr/ar/AziCalculator.java
@@ -0,0 +1,44 @@
+package com.comp30022.arrrrr.ar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by xiaoyuguo on 19/09/2017.
+ */
+
+public class AziCalculator {
+
+    private static double AZIMUTH_ACCURACY = 5;
+
+    /** To compare the calculated and theoretical azimuth */
+    public List<Double> calculateAzimuthAccuracy(double azimuth) {
+        double minAngle = azimuth - AZIMUTH_ACCURACY;
+        double maxAngle = azimuth + AZIMUTH_ACCURACY;
+        List<Double> minMax = new ArrayList<Double>();
+
+        if (minAngle < 0)
+            minAngle += 360;
+
+        if (maxAngle >= 360)
+            maxAngle -= 360;
+
+        minMax.clear();
+        minMax.add(minAngle);
+        minMax.add(maxAngle);
+
+        return minMax;
+    }
+
+    /** To compare the calculated and theoretical azimuth */
+    public boolean isBetween(double minAngle, double maxAngle, double azimuth) {
+        if (minAngle > maxAngle) {
+            if (isBetween(0, maxAngle, azimuth) && isBetween(minAngle, 360, azimuth))
+                return true;
+        } else {
+            if (azimuth > minAngle && azimuth < maxAngle)
+                return true;
+        }
+        return false;
+    }
+}
