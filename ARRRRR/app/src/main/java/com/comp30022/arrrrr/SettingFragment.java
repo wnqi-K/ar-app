39a948a4e608b74ff1945ad8c8dba3029ccf0677
bowenLia <30819747+bowenLia@users.noreply.github.com>
2017-09-29 01:27:25 +1000

    UI update -> edit profile

--- ARRRRR/app/src/main/java/com/comp30022/arrrrr/SettingFragment.java
+++ ARRRRR/app/src/main/java/com/comp30022/arrrrr/SettingFragment.java
@@ -10,6 +10,7 @@ import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
