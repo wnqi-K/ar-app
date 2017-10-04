6373c87e12a4454c154058191c7ad07a066c6db2
zijies1 <546641464@qq.com>
2017-10-04 16:03:07 +1100

    redesign registration for the convenience of implementing registration test

--- ARRRRR/app/src/main/java/com/comp30022/arrrrr/RegisterActivity.java
+++ ARRRRR/app/src/main/java/com/comp30022/arrrrr/RegisterActivity.java
@@ -1,16 +1,51 @@
 package com.comp30022.arrrrr;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.RestrictTo;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
-import com.comp30022.arrrrr.R;
-import com.comp30022.arrrrr.RegisterFragment;
+import com.comp30022.arrrrr.models.User;
+import com.comp30022.arrrrr.utils.Constants;
+import com.comp30022.arrrrr.utils.LoginHelper;
+import com.comp30022.arrrrr.utils.SharedPrefUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class RegisterActivity extends AppCompatActivity {
+/**
+ * This class will perform registration and adding user to Firebase database
+ * functionality.
+ *
+ * @author Zijie Shen
+ */
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG = RegisterActivity.class.getSimpleName();
+    private EditText mETxtEmail, mETxtPassword,mETxtPhonenum,
+            mETxtUsername,mETxtAddress;
+    private Button mBtnRegister;
+    private RadioGroup mRadioGroup;
+    private ProgressDialog mProgressDialog;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mRef;
+    private String gender = null;
 
     public static void startActivity(Context context) {
         Intent intent = new Intent(context, RegisterActivity.class);
@@ -21,16 +56,136 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        init();
+
+        mETxtEmail = (EditText) findViewById(R.id.edit_text_email_id);
+        mETxtPassword = (EditText) findViewById(R.id.edit_text_password);
+        mETxtUsername = (EditText) findViewById(R.id.edit_text_username);
+        mETxtPhonenum = (EditText) findViewById(R.id.edit_text_phonenum);
+        mETxtAddress = (EditText) findViewById(R.id.edit_text_address);
+        mBtnRegister = (Button) findViewById(R.id.button_register);
+        mRadioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+
+        mAuth = FirebaseAuth.getInstance();
+        mRef = FirebaseDatabase.getInstance().getReference();
+
+        mBtnRegister.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        int viewId = view.getId();
 
-    private void init() {
-        // set the register screen fragment
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-        fragmentTransaction.replace(R.id.frame_layout_content_register,
-                RegisterFragment.newInstance(),
-                RegisterFragment.class.getSimpleName());
-        fragmentTransaction.commit();
+        switch (viewId) {
+            case R.id.button_register:
+                onRegister(view);
+                break;
+        }
+    }
+
+    /**
+     * peform Firebase Registration and check whether input is valid or not*/
+    private void onRegister(View view) {
+        String emailId = mETxtEmail.getText().toString();
+        String password = mETxtPassword.getText().toString();
+
+        if (mRadioGroup.getCheckedRadioButtonId() == R.id.radioButton_male) {
+            this.gender = Constants.ARG_MALE;
+        } else if (mRadioGroup.getCheckedRadioButtonId() == R.id.radioButton_female) {
+            this.gender = Constants.ARG_FEMALE;
+        }
+
+        if (gender == null) {
+            Toast.makeText(RegisterActivity.this, "gender required", Toast.LENGTH_SHORT).show();
+        } else if (!LoginHelper.validateForm2(mETxtEmail,
+                mETxtPassword,
+                mETxtUsername,
+                mETxtPhonenum,
+                mETxtAddress)) {
+            // if input is not valid
+            Toast.makeText(RegisterActivity.this, "Error occurs, please check your input", Toast.LENGTH_SHORT).show();
+        } else {
+            performFirebaseRegistration(RegisterActivity.this, emailId, password);
+        }
+    }
+
+    /**
+     * create a Firebase User using email and password
+     * */
+    private void performFirebaseRegistration(Activity activity,
+                                             final String email,
+                                             String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(activity, onRegisterCompleteListener);
+    }
+
+    /**
+     * check whether registration fails or succeeds
+     * */
+    private OnCompleteListener onRegisterCompleteListener = new OnCompleteListener<AuthResult>() {
+        @Override
+        public void onComplete(@NonNull Task<AuthResult> task) {
+            Log.e(TAG, "performFirebaseRegistration:onComplete:" + task.isSuccessful());
+            // If sign in fails, display a message to the user. If sign in succeeds
+            // the auth state listener will be notified and logic to handle the
+            // signed in user can be handled in the listener.
+            if (!task.isSuccessful()) {
+                Toast.makeText(RegisterActivity.this, Constants.ARG_FAILURE, Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getActivity(),task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+            } else {
+                addUserToDatabase(RegisterActivity.this,task.getResult().getUser(),gender);
+            }
+        }
+    };
+
+    /**
+     * add user info to Firebase database
+     * */
+    private void addUserToDatabase(Activity activity, FirebaseUser firebaseUser, String gender){
+        String uid = firebaseUser.getUid();
+        User user = User.createUserWithoutAdmin(uid,
+                firebaseUser.getEmail(),
+                new SharedPrefUtil(activity).getString(Constants.ARG_FIREBASE_TOKEN),
+                mETxtUsername.getText().toString(),
+                mETxtPhonenum.getText().toString(),
+                gender,
+                mETxtAddress.getText().toString());
+        mRef.child(Constants.ARG_USERS)
+                .child(uid)
+                .setValue(user)
+                .addOnCompleteListener(activity,onAddUserToDatabaseCompleteListener);
+    }
+
+    /**
+     * check whether adding class user to Firebase fails or succeeds
+     * */
+    private OnCompleteListener onAddUserToDatabaseCompleteListener = new OnCompleteListener<Void>() {
+        @Override
+        public void onComplete(@NonNull Task<Void> task) {
+            if (!task.isSuccessful()) {
+                Toast.makeText(RegisterActivity.this,
+                        Constants.ARG_FAILURE, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(RegisterActivity.this,
+                        Constants.ARG_SUCCESS, Toast.LENGTH_SHORT).show();
+                EmailLoginActivity.startActivity(RegisterActivity.this);
+
+            }
+        }
+    };
+
+    @RestrictTo(RestrictTo.Scope.TESTS)
+    public OnCompleteListener getOnRegisterCompleteListener() {
+        return onRegisterCompleteListener;
+    }
+
+    @RestrictTo(RestrictTo.Scope.TESTS)
+    public OnCompleteListener getOnAddUserToDatabaseCompleteListener() {
+        return onAddUserToDatabaseCompleteListener;
+    }
+
+    @RestrictTo(RestrictTo.Scope.TESTS)
+    public void setAuth(FirebaseAuth auth) {
+        this.mAuth = auth;
     }
 }
